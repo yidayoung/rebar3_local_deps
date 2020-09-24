cce701af52eb3f15cb218175f9939322d3d76f99
yida_young <418621207@qq.com>
2020-09-24 19:02:48 +0800

    fix error when get hex deps
    Signed-off-by: yida_young <418621207@qq.com>

--- src/rebar3_local_git_resource.erl
+++ src/rebar3_local_git_resource.erl
@@ -5,15 +5,22 @@
 
 
 init(Type, State) ->
+    {ok, Vsn} = application:get_key(rebar, vsn),
+    BaseConfig = #{http_adapter => r3_hex_http_httpc,
+        http_user_agent_fragment =>
+        <<"(rebar3/", (list_to_binary(Vsn))/binary, ") (httpc)">>,
+        http_adapter_config => #{profile => rebar}},
+    Repos = rebar_hex_repos:from_state(BaseConfig, State),
     LocalGitUrl = rebar_state:get(State, local_deps_url, undefined),
-    Resource = rebar_resource_v2:new(Type, ?MODULE, LocalGitUrl),
+    Resource = rebar_resource_v2:new(Type, ?MODULE, #{repos => Repos,
+        base_config => BaseConfig, local_git_url => LocalGitUrl}),
     {ok, Resource}.
 
-lock(AppInfo, LocalGitUrl) ->
+lock(AppInfo, #{local_git_url:=LocalGitUrl}) ->
     AppInfo2 = update_app_info(AppInfo, LocalGitUrl),
     rebar_git_resource:lock(AppInfo2, undefined).
 
-download(TmpDir, AppInfo, State, LocalGitUrl) ->
+download(TmpDir, AppInfo, State, #{local_git_url:=LocalGitUrl}) ->
     AppInfo2 = update_app_info(AppInfo, LocalGitUrl),
     Source = rebar_app_info:source(AppInfo2),
     case Source of
@@ -24,7 +31,7 @@ download(TmpDir, AppInfo, State, LocalGitUrl) ->
     end.
 
 
-needs_update(AppInfo, LocalGitUrl) ->
+needs_update(AppInfo, #{local_git_url:=LocalGitUrl}) ->
     AppInfo2 = update_app_info(AppInfo, LocalGitUrl),
     rebar_git_resource:needs_update(AppInfo2, undefined).
 
@@ -40,9 +47,7 @@ get_resource(AppInfo, LocalGitUrl) ->
     Source = rebar_app_info:source(AppInfo),
     ensure_source(Source, LocalGitUrl).
 
-ensure_source({pkg, PkgName, Vsn, _Hash}, LocalDepUrl) ->
-    ensure_source({git, erlang:binary_to_list(<<PkgName/binary, ".git">>), {tag, Vsn}}, LocalDepUrl);
-ensure_source({pkg, PkgName, Vsn, _OldHash, _Hash}, LocalDepUrl) ->
+ensure_source({pkg, PkgName, Vsn, _OldHash, _Hash, _}, LocalDepUrl) ->
     ensure_source({git, erlang:binary_to_list(<<PkgName/binary, ".git">>), {tag, Vsn}}, LocalDepUrl);
 ensure_source({git_subdir, Url, Checkout, Dir}, LocalDepUrl) ->
     Url2 = ensure_my_dep_dir(Url, LocalDepUrl),
